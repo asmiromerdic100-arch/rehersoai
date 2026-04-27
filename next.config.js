80b6305b545c1fd0d663e4e0e7d2c28fded43dec
asmiromerdic100-arch <asmiromerdic100@gmail.com>
2026-04-27 13:09:44 -0400

    Disable lint and TS checks during production build

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
-/** @type {import('next').NextConfig} */
+﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     serverActions: {
       bodySizeLimit: '5mb',
     },
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
 };
 
